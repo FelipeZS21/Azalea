7d59460119dc6e8731d8350f6a25fca07bc86f3d
FelipeZS21 <felipezasie@hotmail.com>
2023-06-27 22:46:15 -0500

    Clase 7: Componente Card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,12 @@
 import { Layout } from "../../Components/Layout"
-
+import { Card } from "../../Components/Card"
 
 function Home() {
 
     return (
         <Layout>
-            Home 
+            Home
+            <Card></Card>
         </Layout>
     )
 }
