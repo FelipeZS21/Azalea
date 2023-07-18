606f1bc3ac294be03b2a4555865a6a7f5f9a096c
FelipeZS21 <felipezasie@hotmail.com>
2023-07-18 06:45:50 -0500

    Pagina de MyOrder orden individual

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -9,6 +9,9 @@ import { ChevronLeftIcon } from '@heroicons/react/24/outline'
 function MyOrder() {
 
     const context = useContext(ShoppingCartContext)
+    const currentPath = window.location.pathname
+    let index = currentPath.substring(currentPath.lastIndexOf('/') + 1)
+    if(index === 'last') index = context.order?.length - 1 
 
     return (
         <Layout>
@@ -20,7 +23,7 @@ function MyOrder() {
             </div>
             <div className='flex flex-col w-90'>
                 {
-                    context.order?.slice(-1)[0].products.map(product => (
+                    context.order?.[index]?.products.map(product => (
                         <OrderCard 
                             key={product.id}
                             id={product.id}
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -7,6 +7,7 @@ import { ShoppingCartContext } from "../../Context"
 function MyOrders() {
 
     const context = useContext(ShoppingCartContext)
+    console.log(context.order)
 
     return (
         <Layout>
@@ -14,14 +15,14 @@ function MyOrders() {
                 <h1>My Orders</h1>
             </div>
             {
-                context.order.map((order, index) => {
-                    <Link key={index} to={`/my-orders/${order.id}`}>
+                context.order.map((order, index) => (
+                    <Link key={index} to={`/my-orders/${index}`}>
                         <OrdersCard 
                             totalPrice={order.totalPrice}
                             totalProducts={order.totalProducts}
                         />
                     </Link>
-                })
+                ))
             }
         </Layout>
     )
