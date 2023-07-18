82f380640b90e6dbf3922c8000871edadc31b7e8
FelipeZS21 <felipezasie@hotmail.com>
2023-07-17 19:39:53 -0500

    Clase 8: Consumiendo la fakestore

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,30 @@
+import { useState, useEffect} from "react"
 import { Layout } from "../../Components/Layout"
 import { Card } from "../../Components/Card"
 
 function Home() {
 
+    const [items, setItems] = useState(null); 
+
+    useEffect(() =>{
+            fetch('https://fakestoreapi.com/products').then(response => response.json()).then(data => console.log(data))
+        },  
+    []);
+
     return (
         <Layout>
             Home
-            <Card></Card>
-        </Layout>
+            <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+                {
+                    items?.map(item => (
+                    <Card
+                        key={item.id} 
+                        data= {item}
+                    />
+                    ))
+                }
+            </div>
+    </Layout>
     )
 }
 
