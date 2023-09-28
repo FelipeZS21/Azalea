5c65b2e69975f8babb734a31c6163e80b7f50fa6
FelipeZS21 <felipezasie@hotmail.com>
2023-09-28 03:43:37 -0500

    Landing Page: Catalogo

--- src/Components/SomeProducts/SomeProducts.jsx
+++ src/Components/SomeProducts/SomeProducts.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 const SomeProducts = () => {
-  const imagenes = [
-    {/*'../../../Public/Imgs/example1.jpg',
-    '../../../Public/Imgs/example2.jpg',
-    '../../../Public/Imgs/example3.jpg',
-  '../../../Public/Imgs/example4.jpg',*/}
-  ];
-
   return (
-    <h1>hello</h1>
+    <div className="font-Quicksand flex flex-col items-center justify-center">
+        <div className="text-black p-8 flex flex-col justify-center text-center">
+            <h2 className="text-2xl font-semibold">Catalogo</h2>
+            <p className="text-xl mt-2 w-full px-64">
+                Descubre un catálogo de maquillaje que ofrece una variedad 
+                excepcional, con productos de clase, estilo y belleza incomparables, 
+                todo respaldado por una calidad insuperable.
+            </p>
+        </div>
+        <div className='w-full flex justify-center'>
+            <img src="../../../Public/Imgs/labial-logo.png" alt="" className='w-64 h-64'/>
+            <img src="../../../Public/Imgs/brocha-logo.png" alt="" className='w-64 h-64'/>
+        </div>
+    </div>
   );
 };
 
